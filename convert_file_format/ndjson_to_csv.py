fb667f97f7e0f09778ce97d501a72a6bab3cfc61
Nijaguna Darshan <darshannij@gmail.com>
2024-06-16 08:34:09 +0100

    add tqdm

--- convert_file_format/ndjson_to_csv.py
+++ convert_file_format/ndjson_to_csv.py
@@ -3,10 +3,16 @@ import pandas as pd
 import json
 
 def ndjson_to_csv(ndjson_path, csv_path):
+    # get the total number of lines for progress bar
+    total_lines = sum(1 for line in open(ndjson_path, 'r', encoding='utf-8'))
+    
     raw_data = []
     with open(ndjson_path, 'r', encoding='utf-8') as ndjson:
-        for line in tqdm([ndjson]):
-            json_line = json.loads(line.strip())
+        # read every line as json obj and append to list
+        for line in tqdm(ndjson, total=total_lines, desc="Processing"):
+            json_line = json.loads(line)
             raw_data.append(json_line)
+    
+    # convert list to CSV and save
     df = pd.DataFrame(raw_data)
-    df.to_csv(csv_path)
+    df.to_csv(csv_path, index=False)
